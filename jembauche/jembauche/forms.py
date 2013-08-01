f59f7b11fcfda11fac61a821299eb4b0a2689744
sdeprez <sdeprez@multiposting.fr>
2013-08-01 16:44:20 +0200

    Forms updated

--- jembauche/jembauche/forms.py
+++ jembauche/jembauche/forms.py
@@ -16,40 +16,48 @@ class OfferForm(forms.Form):
     title = forms.CharField(
         required=True,
         widget=forms.TextInput(attrs={'class': 'form-control'}),
+        label=u'Titre de l\'offre',
     )
     name_company = forms.CharField(
         required=True,
         widget=forms.TextInput(attrs={'class': 'form-control'}),
+        label=u'Nom de l\'entreprise',
     )
     num_employee = forms.ChoiceField(
         choices=_employee_choices,
         required=True,
         widget=forms.Select(attrs={'class': 'form-control'}),
+        label=u'Nombre d\'employés',
     )
     localisation = forms.CharField(
         required=True,
         widget=forms.TextInput(attrs={'class': 'form-control'}),
+        label=u'Localisation de l\'offre',
     )
     description = forms.CharField(
         required=True,
         widget=forms.Textarea(attrs={'class': 'form-control'}),
+        label=u'Description de l\'offre',
     )
     email = forms.EmailField(
         required=True,
         widget=forms.TextInput(attrs={'class': 'form-control'}),
+        label=u'Email',
     )
 
     nom_contact = forms.CharField(
         required=True,
         widget=forms.TextInput(attrs={'class': 'form-control'}),
+        label=u'Nom',
     )
     prenom_contact = forms.CharField(
         required=True,
         widget=forms.TextInput(attrs={'class': 'form-control'}),
+        label=u'Prénom',
     )
     phone_contact = forms.RegexField(
         regex=_phone_re,
         required=True,
         widget=forms.TextInput(attrs={'class': 'form-control'}),
+        label='Numéro de téléphone',
     )
-
