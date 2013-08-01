016daa406e6c6b1bb177e4e1ae128e50ab8262b9
Alexandre Sadones <asadones@multiposting.fr>
2013-08-01 16:35:30 +0200

    Structured form for BootStrap

--- jembauche/jembauche/forms.py
+++ jembauche/jembauche/forms.py
@@ -13,17 +13,43 @@ class OfferForm(forms.Form):
     )
     _phone_re = r'^0[1-6]{1}(([0-9]{2}){4})|((\s[0-9]{2}){4})|((-[0-9]{2}){4})$'
 
-    title = forms.CharField(required=True)
-    name_company = forms.CharField(required=True)
-    num_employee = forms.MultipleChoiceField(
+    title = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
+    name_company = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
+    num_employee = forms.ChoiceField(
         choices=_employee_choices,
         required=True,
+        widget=forms.Select(attrs={'class': 'form-control'}),
     )
-    localisation = forms.CharField(required=True)
-    description = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
+    localisation = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
+    description = forms.CharField(
+        required=True,
+        widget=forms.Textarea(attrs={'class': 'form-control'}),
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
 
-    nom_contact = forms.CharField(required=True)
-    prenom_contact = forms.CharField(required=True)
-    phone_contact = forms.RegexField(regex=_phone_re, required=True)
+    nom_contact = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
+    prenom_contact = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
+    phone_contact = forms.RegexField(
+        regex=_phone_re,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
 
