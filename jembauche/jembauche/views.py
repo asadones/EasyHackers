9e6c7c2d1a5ad359b6550830c58ed6c5918370b5
Alexandre Sadones <asadones@multiposting.fr>
2013-08-01 19:03:16 +0200

    Added errors on company name

--- jembauche/jembauche/views.py
+++ jembauche/jembauche/views.py
@@ -9,11 +9,17 @@ class HomepageView(TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(HomepageView, self).get_context_data()
-        context['contact_form'] = ContactForm()
-        context['offer_form'] = OfferForm()
+        if self.request.method == 'POST':
+            context['offer_form'] = OfferForm(self.request.POST)
+        else:
+            context['offer_form'] = OfferForm()
         return context
 
     def get(self, *args, **kwargs):
+        context = self.get_context_data(*args, **kwargs)
+        return self.render_to_response(context)
+
+    def post(self, *args, **kwargs):
         context = self.get_context_data(*args, **kwargs)
         return self.render_to_response(context)
 
